baf2159ccb51f95ebef56a84965518a02437b57d
Rakib Islam <rakib1987@gmail.com>
2020-11-30 19:48:25 -0500

    Init Rails app with tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+// module.exports = {
+//   purge: [],
+//   darkMode: false, // or 'media' or 'class'
+//   theme: {
+//     extend: {},
+//   },
+//   variants: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
+
+const defaultTheme = require('tailwindcss/defaultTheme')
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter', ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [
+    require('@tailwindcss/ui'),
+  ]
+}
